88b9d58a78bd67bb0f52bb9c8389568b9995b253
huberc <carolin-elisabeth.huber@ufz.de>
2021-02-16 16:54:03 +0100

    set global variables

---
+++ test/input_files/globalvar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+glob_workdir="/work/$USER/incubatoR_test"
+glob_mode="Pos"
+glob_filedir="/home/$USER/testdata/mzml"
+glob_Rscripts="/home/$USER/incubatoR/Rscripts"
+glob_setttings="/home/$USER/incubatoR/test/input_files"
+
