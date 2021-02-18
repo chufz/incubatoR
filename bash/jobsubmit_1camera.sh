266fed129a2c0da9a13bfda11bca50ffda5afdc5
huberc <carolin-elisabeth.huber@ufz.de>
2021-02-18 13:58:28 +0100

    jobsubmit xcms and camera

---
+++ bash/jobsubmit_1camera.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source globalvar.sh
+
+for i in $glob_mode
+	do
+		echo "module load Anaconda3
+		source activate $glob_ENV
+		Rscript $glob_Rscripts/camera.R $glob_input/$i/settings_camera.yaml $glob_workdir/$i/xcms.rds $i $glob_input/$i" | qsub -S /bin/bash -l h_rt=10:00:00 -l h_vmem=10G, -binding linear:1 -cwd -m e -N camera_$i
+	done
--- test/input_files/globalvar.sh
+++ test/input_files/globalvar.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                       
+glob_ENV="r_env" # conda environment with requirements                                                                                                 
+glob_workdir="/work/$USER/incubatoR_test" # define working directory                                                                              
+glob_mode="Pos" # ion mode                                                                                                                        
+glob_filedir="/home/$USER/testdata/mzml" # folder with mzml-files                                                                                 
+glob_Rscripts="/home/$USER/incubatoR/Rscripts" # folder with Rscripts                                                                             
+glob_input="/home/$USER/incubatoR/test/input_files" # folder containing input files                                                           
+                                                                          
 
-glob_workdir="/work/$USER/incubatoR_test"
-glob_mode="Pos"
-glob_filedir="/home/$USER/testdata/mzml"
-glob_Rscripts="/home/$USER/incubatoR/Rscripts"
-glob_setttings="/home/$USER/incubatoR/test/input_files"
+
 
