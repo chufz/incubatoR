59f5b4403cbf30e71250dec8c01e29400a6387dd
huberc <huberc@ufz.de>
2021-02-18 14:27:37 +0100

    run xcms

--- bash/jobsubmit_1xcms.sh
+++ bash/jobsubmit_1xcms.sh
@@ -5,8 +5,9 @@ mkdir $glob_workdir/
 for i in $glob_mode
 	do
 		mkdir $glob_workdir/$i
-		echo "module load Anaconda3
+		echo "
+		module load Anaconda3
 		source activate $glob_ENV
-		Rscript $glob_Rscripts/xcms.R $glob_input/$i/settings_xcms.yaml $glob_filedir/$i/class.csv $glob_workdir/$i" | qsub -S /bin/bash -l h_rt=10:00:00 -l h_vmem=10G, -binding linear:1 -cwd -m e -N xcms_$i
+		Rscript $glob_Rscripts/xcms.R $glob_input/$i/settings_xcms.yaml $glob_filedir/$i/class.csv $glob_workdir/$i " | qsub -S /bin/bash -l h_rt=10:00:00 -l h_vmem=10G, -binding linear:1 -cwd -m e -N xcms_$i
 	done
 
