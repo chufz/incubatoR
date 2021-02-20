ada43c1243a1ce986d96726b4b38328f67ba617e
huberc <huberc@ufz.de>
2021-02-20 01:04:30 +0100

    change genform FF directory

--- bash/jobsubmit_6genform.sh
+++ bash/jobsubmit_6genform.sh
@@ -16,8 +16,7 @@ for h in $glob_mode
         	for i in $(find $j  -iname "*.txt" | grep -v "MS1.txt")
 		do
                 FOLDER=$( dirname "${i}" )
-		echo "genform  ms=$FOLDER/MS1.txt msms=$i ff=$(cat $( dirname "${FOLDER}")/ff) oclean=$(echo $i"_clean") out=$(echo $i | sed 's/.txt$/.out/') exist analyze loss wi=sqrt ppm=8 acc=20 rej=10 sort=msmsmv oei"  
+		echo "genform  ms=$FOLDER/MS1.txt msms=$i ff=$(cat $glob_input/$h/$i/FF_$(echo $i).txt ) oclean=Clean_$i out=$(echo $i | sed 's/.txt$/.out/') exist analyze loss wi=sqrt ppm=8 acc=20 rej=10 sort=msmsmv oei"  
 		done  |  qsub -S /bin/bash -l h_rt=1:00:00 -l h_vmem=6G, -binding linear:1	
 	done
-
 	done
