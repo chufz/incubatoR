1b592a449f81f81eb9cd0153f0ddab8d45f313ab
Carolin Huber <38219063+chufz@users.noreply.github.com>
2021-02-25 14:01:00 +0100

    Update jobsubmit_6genform.sh
    Add variables from parameter file

--- bash/jobsubmit_6genform.sh
+++ bash/jobsubmit_6genform.sh
@@ -2,6 +2,7 @@
 source globalvar.sh
 
 for h in $glob_mode
+	source $glob_input/$h/parameter_genform.sh
 	do
         	if [ $h = "Pos" ]; then
 		ION="M+H"
@@ -16,7 +17,7 @@ for h in $glob_mode
         	for i in $(find $j  -iname "*.txt" | grep -v "MS1.txt")
 		do
                 FOLDER=$( dirname "${i}" )
-		echo "genform  ms=$FOLDER/MS1.txt msms=$i ff=$(cat $glob_input/$h/$i/FF_$(echo $i).txt ) oclean=Clean_$i out=$(echo $i | sed 's/.txt$/.out/') exist analyze loss wi=sqrt ppm=8 acc=20 rej=10 sort=msmsmv oei"  
+		echo "genform  ms=$FOLDER/MS1.txt msms=$i ff=$(cat $glob_input/$h/$i/FF_$(echo $i).txt ) oclean=Clean_$i out=$(echo $i | sed 's/.txt$/.out/') exist analyze loss wi=sqrt ppm=$PPM acc=$ACC rej=$REJ sort=msmsmv oei"  
 		done  |  qsub -S /bin/bash -l h_rt=1:00:00 -l h_vmem=6G, -binding linear:1	
 	done
 	done
