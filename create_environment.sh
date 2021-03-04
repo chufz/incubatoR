9c9c56bc28ecefd3f3ee2296a8969e66deae5deb
M Bernt <m.bernt@ufz.de>
2021-03-04 15:46:08 +0100

    Update create_environment.sh
    Its better to use R from conda-forge and to install R packages from conda as well (if they exist there).

--- create_environment.sh
+++ create_environment.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 module load Anaconda3
-conda create --name  incubatoR
-source activate incubatoR
-conda install r-essentials r-base
-conda config --add channels bioconda
-conda install -c bioconda genform 
-conda install -c bioconda r-yaml
-
-Rscript install_dependencies.R
+conda create --name  incubatoR -c conda-forge -c biopython r-essentials r-base genform r-yaml bioconductor-xcms bioconductor-camera bioconductor-mspurity
