d41d197cce04332a53a7c74a95079d38cb99cfd8
Carolin Huber <38219063+chufz@users.noreply.github.com>
2021-03-04 12:25:21 +0100

    Update jobsubmit_5ddextract.sh

--- bash/jobsubmit_5ddextract.sh
+++ bash/jobsubmit_5ddextract.sh
@@ -12,6 +12,6 @@ for h in $glob_mode
 		echo "
 		module load Anaconda3
 		source activate $glob_ENV
-		Rscript $glob_Rscripts/ddextract.R $glob_workdir/$h/$i/Metabolites.txt $glob_filedir/$h  $glob_workdir/$h/$i/MSMS 0.005 20 $(echo $i)_R 0.4 " # |  qsub -N EIC_$i  -l h_rt=00:40:00 -l h_vmem=10G -binding linear:1 #-o $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log -e $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log  
+		Rscript $glob_Rscripts/ddextract.R $glob_workdir/$h/$i/Metabolites.txt $glob_filedir/$h  $glob_workdir/$h/$i/MSMS 0.005 20 $(echo $i)_R 0.4 "  |  qsub -N EIC_$i  -l h_rt=00:40:00 -l h_vmem=10G -binding linear:1 #-o $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log -e $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log  
 		done
 done  
