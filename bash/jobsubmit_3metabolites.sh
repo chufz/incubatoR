e97b855837c7538193146a1be00334790195d117
Carolin Huber <38219063+chufz@users.noreply.github.com>
2021-03-04 12:27:11 +0100

    Update jobsubmit_3metabolites.sh

--- bash/jobsubmit_3metabolites.sh
+++ bash/jobsubmit_3metabolites.sh
@@ -8,10 +8,10 @@ do
         CLASS=$(cat $CLASSFILE | cut -d "," -f 2 | grep "_clean" | sed 's/_clean//g')
         for i in $CLASS
         do
-                # echo "
+                echo "
                 module load Anaconda3
                 source activate $glob_ENV
-                Rscript $glob_Rscripts/metabolites.R $glob_workdir/$h/$i/Result_$(echo $i)_R.rds $glob_workdir/$h/$i $glob_input/$h/$i/target_$(echo $i).txt $glob_input/$h/$i/parent_$(echo $i).txt $glob_input/$h/$i/RT_$(echo $i).txt $glob_input/$h/settings_metabolites.yaml $h #"   |  qsub -N statistic_$h_$i  -l h_rt=00:20:00 -l h_vmem=10G -binding linear:1  -o $glob_workdir/$h/$JOB_ID.log -e $glob_workdir/$h/$JOB_ID.log  
+                Rscript $glob_Rscripts/metabolites.R $glob_workdir/$h/$i/Result_$(echo $i)_R.rds $glob_workdir/$h/$i $glob_input/$h/$i/target_$(echo $i).txt $glob_input/$h/$i/parent_$(echo $i).txt $glob_input/$h/$i/RT_$(echo $i).txt $glob_input/$h/settings_metabolites.yaml $h "   |  qsub -N statistic_$h_$i  -l h_rt=00:20:00 -l h_vmem=10G -binding linear:1  -o $glob_workdir/$h/$JOB_ID.log -e $glob_workdir/$h/$JOB_ID.log  
 
         done
 done
