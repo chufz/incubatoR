407ce685ac0574ee4d06c5aba8403d849a1249ae
Carolin Huber <38219063+chufz@users.noreply.github.com>
2021-03-04 12:26:23 +0100

    Update jobsubmit_4eic.sh

--- bash/jobsubmit_4eic.sh
+++ bash/jobsubmit_4eic.sh
@@ -12,6 +12,6 @@ for h in $glob_mode
 		echo "
 		module load Anaconda3
 		source activate $glob_ENV
-		Rscript $glob_Rscripts/eic.R $glob_workdir/$h/$i/Metabolites.txt $(echo $i)_R 'NC' $(echo $i)_clean $glob_filedir/$h $glob_workdir/$h/$i/EIC 4 0.001 T T " # |  qsub -N EIC_$i  -l h_rt=00:40:00 -l h_vmem=10G -binding linear:1 #-o $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log -e $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log  
+		Rscript $glob_Rscripts/eic.R $glob_workdir/$h/$i/Metabolites.txt $(echo $i)_R 'NC' $(echo $i)_clean $glob_filedir/$h $glob_workdir/$h/$i/EIC 4 0.001 T T "  |  qsub -N EIC_$i  -l h_rt=00:40:00 -l h_vmem=10G -binding linear:1 #-o $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log -e $PROCESSFOLDER/joboutput_preprocessing/$JOB_ID.log  
 		done
 done  
